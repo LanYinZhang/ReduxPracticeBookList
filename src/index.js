581de68f1c32ebb241a0e2bfd3d09063e9a11466
Lanyin <lanyinpoppy@gmail.com>
2017-12-31 21:37:40 -0500

    add booklist container

--- src/index.js
+++ src/index.js
@@ -1,45 +1,15 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import YTAPI from 'youtube-api-search';
-const API_KEY = 'AIzaSyCpK_wK0Jrlj5voi8u9JLd0gGhQLKhwbp8';
+import App from './components/app';
+import reducers from './reducers';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            videos : [],
-            selectedVideo : null
-        };
-        this.searchTerm('surfBoards');
-    }
-    searchTerm(term) {
-        YTAPI({key : API_KEY, term : term}, (videos) => {
-            this.setState({
-                videos : videos,
-                selectedVideo : videos[0]
-            });
-
-        });
-    }
-
-    slow = _.debounce(term => this.searchTerm(term), 300);
-
-    render() {
-        return <div>
-            <SearchBar onSearch = {this.slow}/>
-            <VideoDetail
-                video = {this.state.selectedVideo}/>
-            <VideoList videos = {this.state.videos}
-                       onSelectVideo = {selectedVideo => this.setState({selectedVideo}) }
-            />
-        </div>;
-    }
-}
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+    , document.querySelector('.container'));
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        {title : 'Harry Porter'},
+        {title : 'intro to java'},
+        {title : 'happy coding'},
+        {title : 'learn cooking'},
+        {title : 'web programming'}
+    ];
+}
